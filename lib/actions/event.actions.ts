0354d82a2f2dce4d30ccd92840ca0a1b4b54fb64
unknown <gadeaniruddha2@gmail.com>
2024-01-29 14:44:13 +0530

    add: event detail page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,8 @@ import { CreateEventParams } from '@/types'
 import { connectToDatabase } from '@/lib/database'
 import User from '../database/models/user.model'
 import Event from '../database/models/event.model'
+import Category from '@/lib/database/models/category.model'
+import { handleError } from '../utils'
 
 export const createEvent = async ({ event, userId, path }: CreateEventParams) => {
     try {
@@ -25,7 +27,30 @@ export const createEvent = async ({ event, userId, path }: CreateEventParams) =>
 
     catch (error) {
         console.log("Erro while creating event ")
-        console.log(error)
+        handleError(error)
 
+    }
+}
+
+
+const populateEvent = (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' });
+}
+
+export const getEventDetailsById = async (eventId: string) => {
+    try {
+        await connectToDatabase();
+
+        const event = await populateEvent(Event.findById(eventId));
+        if (!event) {
+            throw new Error("Event data is not found")
+        }
+        return JSON.parse(JSON.stringify(event));
+    }
+    catch (error) {
+        console.log("Erro while fetching event data by Id ")
+        handleError(error)
     }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -87,7 +87,9 @@ export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryPara
   )
 }
 
+
+
 export const handleError = (error: unknown) => {
-  console.error(error)
-  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
+  console.error(error);
+  throw new Error(typeof error === 'string' ? error : JSON.stringify(error));
 }
