802c05f7e9300ae2684405ff420773df9c4c3e58
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-03-29 20:22:09 +0530

    enhance UI for not found data

--- app/(root)/events/find/page.tsx
+++ app/(root)/events/find/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import SearchEventForm from '@/components/shared/SearchEventForm';
 import { useSearchEventContext } from '@/app/context/searchEventContext';
 import EventCard2 from '@/components/shared/EventCard2';
+import Image from 'next/image';
 
 
 // Loading Skeleton
@@ -22,27 +23,7 @@ const LoadingSkeleton = () => {
 const page = () => {
 
     const { searchEventData, searchEventLoading } = useSearchEventContext()
-    console.log("from context searchEventData = ", searchEventData)
-    const tempData = [
-        {
-            event_name: 'Smart India Hackathon 2024',
-            city_name: 'Delhi',
-            date: '2024-04-05T11:00:53.000Z',
-            weather: 'Sunny, 18C',
-            distance_km: '1143.6432140846102',
-            imageUrl: 'https://utfs.io/f/e673ddfd-10c3-4034-a6c5-041d52a9d0ba-5hcmx7.png',
-            eventId: '66069fdf1f3cdea91919cbe7'
-        },
-        {
-            event_name: 'Patna Festival 2024',
-            city_name: 'Patna',
-            date: '2024-04-06T11:00:53.000Z',
-            weather: 'Sunny, 18C',
-            distance_km: '1443.9477535510216',
-            imageUrl: 'https://utfs.io/f/9a245f91-fda6-4f98-ad30-e29551526f56-5hcnlf.png',
-            eventId: '6606a03b1f3cdea91919cbef'
-        }
-    ]
+    console.log("search Event Data = ", searchEventData)
 
 
     return (
@@ -71,17 +52,26 @@ const page = () => {
                             <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 xl:gap-10'>
                                 {Array.from({ length: 6 }, (_, index) => <LoadingSkeleton key={index} />)}
                             </div>
-                        ) : tempData.length > 0 ? (
+                        ) : searchEventData.length > 0 ? (
                             <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 xl:gap-10'>
                                 {
-                                    tempData.map((event) => (
+                                    searchEventData.map((event) => (
                                         <EventCard2 event={event} />
                                     ))
                                 }
                             </div>
                         ) : (
                             <div className='flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-500/20 py-28 text-center'>
-                                <h3 className='p-bold-20 md:h5-bold'>Events Not Found..!</h3>
+                                <div className='flex items-center gap-3 p-bold-20 md:h5-bold'>
+                                    <p>Events Not Found..!</p>
+                                    <Image
+                                        src="/assets/icons/sad.png"
+                                        width={40}
+                                        height={40}
+                                        alt="sad emoji"
+                                        className='object-contain'
+                                    />
+                                </div>
                                 <p className='p-regular-14'>Please update date</p>
                             </div>
                         )
